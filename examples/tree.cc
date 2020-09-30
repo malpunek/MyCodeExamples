79d5361a0b7c566bc3632cfbe5b6872c2630ce59
Stanisław Morawski <malpunek@gmail.com>
2020-09-30 09:56:27 +0200

    IntervalTree example

---
+++ examples/tree.cc
@@ -0,0 +1,18 @@
+#include <bits/stdint-intn.h>
+#include <iostream>
+
+#include "libalgo/IntervalTree.hh"
+
+int main() {
+    auto x = libalgo::IntervalTree<int64_t, int>(0, (1ll<<60));
+    int64_t big_num = 1ll<<59;
+    x.add(10, 10, big_num);
+    auto t1 = x.query(2, 10);
+    auto t2 = x.query(2, 15);
+    auto t3 = x.query(big_num - 1, big_num * 2);
+    auto t4 = x.query(big_num, big_num * 2);
+    std::cout << t1 << " " << t2 << std::endl;
+    std::cout << t3 << " " << t4 << std::endl;
+    std::cout << "SIZE: " << x.size() << std::endl;
+    return 0;
+}
